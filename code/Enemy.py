93c8a265b1e3bbfb8329e18e64b82978deac7f75
Ricardo Souza <ricardo3355@gmail.com>
2024-09-03 20:28:36 -0300

    Final Commit

--- code/Enemy.py
+++ code/Enemy.py
@@ -13,14 +13,14 @@ class Enemy(Entity):
 
     def move(self):
         if self.name == 'Enemy3':
-            # Move horizontally from right to left
+            # Movimento horizontal da direita para esquerda
             self.rect.centerx -= ENTITY_SPEED[self.name]
-            # Vertical movement logic for Enemy3
+            # Movimento vertical para o Inimigo 3
             if self.rect.top <= 0:  # Hits the top border
-                # Increase speed downwards
+                # Velocidade de descida do inimigo 3
                 self.vertical_speed = abs(self.vertical_speed) * 2
-            elif self.rect.bottom >= WIN_HEIGHT:  # Hits the bottom border
-                self.vertical_speed = -abs(self.vertical_speed)  # Move upwards
+            elif self.rect.bottom >= WIN_HEIGHT:  # limite borda inferior
+                self.vertical_speed = -abs(self.vertical_speed)  # Movimento superior
             self.rect.centery += self.vertical_speed
         else:
             self.rect.centerx -= ENTITY_SPEED[self.name]
@@ -36,18 +36,18 @@ class Enemy3(Enemy):
     def __init__(self, position: tuple):
         super().__init__('Enemy3', position)
         self.speed_y = 2
-        self.direction_y = 1  # 1 for moving down, -1 for moving up
+        self.direction_y = 1  # 1 move-se para baixo, -1 para cima
         self.shoot_asset = pygame.image.load('./asset/Enemy3shot.png')
 
     def move(self):
-        self.rect.x -= 0.9  # Move horizontal
+        self.rect.x -= 0.9  # Move-se horizontal
 
-        # Move vertical with up and down pattern
+        # Move-se verticalmente com padrão cima e baixo
         self.rect.y += self.speed_y * self.direction_y
         if self.rect.bottom > WIN_HEIGHT:
-            self.direction_y = -1  # Move up
+            self.direction_y = -1  # Move-se pra cima
         if self.rect.top < 0:
-            self.direction_y = 3  # Move down
+            self.direction_y = 3  # Move-se para baixo
 
     def shoot(self):
         self.shot_delay -= 1
